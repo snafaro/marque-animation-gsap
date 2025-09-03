8e31efc76236058d1a6d60e2496405c936b02256
shahriar nafis <snafaro02@gmail.com>
2025-09-03 13:12:06 +0600

    animation created with wheel response

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("marque animation");
+
+
+window.addEventListener("wheel",(dets)=>{
+    if(dets.deltaY > 0){
+        gsap.to("#container",{
+            transform: "translateX(-100%)",
+            duration: 3,
+            repeat: -1,
+            ease: "none"
+        })
+    }
+    else{
+        gsap.to("#container",{
+            transform: "translateX(0%)",
+            duration: 3,
+            repeat: -1,
+            ease: "none"
+        })
+    }
+})
+
+
+
