e0754ea91ae852c1534d0dc28926c6f11ca23098
shahriar nafis <snafaro02@gmail.com>
2025-09-03 13:21:56 +0600

    animation complete

--- script.js
+++ script.js
@@ -3,19 +3,25 @@ console.log("marque animation");
 
 window.addEventListener("wheel",(dets)=>{
     if(dets.deltaY > 0){
-        gsap.to("#container",{
+        gsap.to("#marque",{
             transform: "translateX(-100%)",
-            duration: 3,
+            duration: 1,
             repeat: -1,
             ease: "none"
         })
+        gsap.to("#marque img",{
+            rotate: 180
+        })
     }
     else{
-        gsap.to("#container",{
+        gsap.to("#marque",{
             transform: "translateX(0%)",
-            duration: 3,
+            duration: 1,
             repeat: -1,
             ease: "none"
+        })
+        gsap.to("#marque img",{
+            rotate: 0
         })
     }
 })
